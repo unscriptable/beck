45408f59dd50b4fcc73f773aff4d158b6f83769b
unscriptable <john@unscriptable.com>
2013-07-05 13:33:52 -0400

    Add fetchText module

---
+++ shim/fetchText.js
@@ -0,0 +1,22 @@
+(function () {
+
+	function fetchText (url, callback, errback) {
+		var xhr;
+		xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4) {
+				if (xhr.status < 400) {
+					callback(xhr.responseText);
+				}
+				else {
+					errback(new Error('fetchText() failed. status: ' + xhr.status + ' - ' + xhr.statusText));
+				}
+			}
+		};
+		xhr.send(null);
+	}
+
+	System.set('beck/shim/fetchText', ToModule(fetchText));
+
+}());
