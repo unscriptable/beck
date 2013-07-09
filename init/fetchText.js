4f1da4b56bde6fc0117864a2de14a87e93e85661
unscriptable <john@unscriptable.com>
2013-07-08 20:02:41 -0400

    Add node/ringo XMLHttpRequest alternative. Remove IE6 shim.

--- init/fetchText.js
+++ init/fetchText.js
@@ -1,6 +1,20 @@
-(function () {
+(function (scopedRequire) {
 
-	function fetchText (url, callback, errback) {
+	var fs, fetchText;
+
+	// determine the correct method upon first use
+	fetchText = function (url, callback, errback) {
+		fetchText = hasXhr() ? xhrFetch : hasFsModule() ? fsFetch : failFetch;
+		return fetchText(url, callback, errback);
+	};
+
+	System.set('beck/init/fetchText', ToModule(fetch));
+
+	function fetch (url, callback, errback) {
+		return fetchText(url, callback, errback);
+	}
+
+	function xhrFetch (url, callback, errback) {
 		var xhr;
 		xhr = new XMLHttpRequest();
 		xhr.open('GET', url, true);
@@ -17,6 +31,35 @@
 		xhr.send(null);
 	}
 
-	System.set('beck/init/fetchText', ToModule(fetchText));
+	function fsFetch (url, callback, errback) {
+		fs.readFile(url, function (err) {
+			if (err) {
+				errback(err);
+			}
+			else {
+				callback.apply(this, slice.call(arguments, 1));
+			}
+		});
+	}
 
-}());
+	function failFetch () {
+		throw new Error('Could not create a text file fetcher.');
+	}
+
+	function hasXhr () {
+		return typeof XMLHttpRequest != 'undefined';
+	}
+
+	function hasFsModule () {
+		var slice;
+		if (scopedRequire) {
+			slice = Array.prototype.slice;
+			try {
+				fs = scopedRequire('fs');
+				return true;
+			}
+			catch (ex) { }
+		}
+	}
+
+}(typeof require == 'function' && require));
