d781d11eeaef0730127e970fe01b82911f9371df
unscriptable <john@unscriptable.com>
2013-07-09 06:30:37 -0400

    Rename load-node to load_ssjs

--- test/load_ssjs.js
+++ test/load_ssjs.js
@@ -1,7 +1,7 @@
 require('../load');
 System.load('run',
 	function (run) {
-		console.log('this is the main module: ', run);
+		console.log('this is the run module: ', run);
 	},
 	function (ex) {
 		console.error('oops!', ex);
